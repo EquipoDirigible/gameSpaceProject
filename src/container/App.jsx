7ac7b3e2b0b9ca0b20c8c76eee764c632bcda00c
EvaAramburu <evamariadguez@gmail.com>
2023-02-27 14:28:04 +0100

    added background

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../components/stylesheets/reset.css'
-
-import { Container  } from 'react-bootstrap'                                                                                    
+                                                                                  
 import ListProducts from '../components/ListProducts'
 import NewNavbar from '../components/NewNavBar'
 import '../container/App.css'
@@ -9,11 +8,11 @@ import '../container/App.css'
 const App = () => {
   return (
     
-    <> 
+    <div className="container-background"> 
     <NewNavbar/> 
       <h1 className="text-center"> List of products  </h1>
       <ListProducts/>
-    </>
+    </div>
 
   )
 }
