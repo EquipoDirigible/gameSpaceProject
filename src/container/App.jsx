6625a4150b8dfe8bbcd119c008c319e386db06a6
Ana Garrido <anagarridocoder@gmail.com>
2023-02-27 15:03:52 +0100

    footer

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './App.css'
+import Footer from '../components/Footer'
 import { Container  } from 'react-bootstrap'                                                                                    
 import ListProducts from '../components/ListProducts'
 
+
 const App = () => {
   return (
+    
     <Container fluid> 
       <h1 className="text-center"> List of products  </h1>
       <ListProducts/>
+      <Footer/>
     </Container>
+    
   )
 }
 export default App;
