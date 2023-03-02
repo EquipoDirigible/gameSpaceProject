c7bf72bed8925f1a33ee66e7f6972b52c5f03605
MartaMunMol <marmunmol@gmail.com>
2023-03-02 12:18:14 +0100

    merge footer branch fixed

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-<<<<<<< HEAD
 import './App.css'
 import Footer from '../components/Footer'
 import { Container  } from 'react-bootstrap'                                                                                    
-=======
 import '../components/stylesheets/reset.css'
                                                                                   
->>>>>>> feature/cardView
 import ListProducts from '../components/ListProducts'
 import NewNavbar from '../components/NewNavBar'
 import '../container/App.css'
@@ -14,24 +11,15 @@ import '../container/App.css'
 
 const App = () => {
   return (
-<<<<<<< HEAD
     <>
     <div> 
+      <NewNavbar/>
       <h1 className="text-center"> List of products  </h1>
       <ListProducts/>
       <Footer/>
     </div>
     
     </>
-=======
-    
-    <div className="container-background"> 
-    <NewNavbar/> 
-      <h1 className="text-center"> List of products  </h1>
-      <ListProducts/>
-    </div>
-
->>>>>>> feature/cardView
   )
 }
 export default App;
